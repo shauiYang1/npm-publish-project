438bc7c9065c5ca55d49b6876c7a60b74abaa2de
yangshuai <u9h_75bugk181@dingtalk.com>
2023-02-07 16:05:19 +0800

    去除本地packages引入

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import packages from "../packages";
 
-createApp(App).use(ElementPlus).use(packages).mount("#app");
+createApp(App).use(ElementPlus).mount("#app");
